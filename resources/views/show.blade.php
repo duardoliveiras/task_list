55d4bffb0da54c166f26c65d0364a5f3a02a7934
Eduardo Oliveira <duardooliveiras@gmail.com>
2023-12-03 15:42:32 +0000

    docker_compose

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,7 +5,16 @@
 @section('content')
 
     <h1> {{$task->title}} </h1>
-    <p> {{$task->description}}</p>
-    <p> {{$task->createdAt}}</p>
+    <p> <b>Description:</b> {{$task->description}}</p>
+
+    @if($task->longDescription)
+        <p> <b>Long Description:</b> {{$task->longDescription}}</p>
+    @endif
+    <p> <b>Created At:</b> {{$task->createdAt}}</p>
+
+    @if($task->completed)
+        <p> <b>Completed At:</b> {{$task->completedAt}}</p>
+    @endif
+
 
 @endsection
